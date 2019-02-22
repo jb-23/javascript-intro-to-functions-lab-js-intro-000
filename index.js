771e16b285f37eb4d0106320cc4b0eb82406ba19
Jason Bamford <scarfandgoggles@yahoo.co.uk>
2019-02-22 20:46:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
